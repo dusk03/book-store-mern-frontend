c04da18c23cadaac83d3b6b57f284605984e4fc9
Dang_Thang <dinhthanghp03@gmail.com>
2024-12-21 22:32:15 +0700

    config tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <nav>Navbar</nav>
-    <main className="min-h-screen">
+    <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
     <Outlet/>
     </main> 
       
