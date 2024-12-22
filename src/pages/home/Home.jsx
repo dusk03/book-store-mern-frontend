8d5e095ed9a399b3612723e4ffbb3fac64817423
Dang_Thang <dinhthanghp03@gmail.com>
2024-12-22 22:28:54 +0700

    topseller section layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
+import TopSallers from './TopSallers'
+import Recommened from './Recommened'
 
 function Home() {
   return (
-    <Banner/>
+    <>
+        <Banner/>
+        <TopSallers/>
+        <Recommened/>
+    </>
+
   )
 }
 
